fecadfb5dbed06fbd3eb51f329868c4ecb9895a2
Tucker <tuckerhoog@tutanota.com>
2021-07-25 21:33:31 -0400

    add backgrounds

---
+++ src/views/BCNM-KSNM-ENM.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class = "bcnm-ksnm-enm">
+    <h2>Working BCNMs, KSNMs, and ENMs</h2>
+      <div class = "meat">
+        <div>
+          <h2>Balgas Dais</h2>
+          <p>Treasure and Tribulations (BS50)</p>
+          <p>Wild Wild Whiskers (BS60)</p>
+          <p>Divine Punishers (BS60)</p>
+          <p>Early Bird Catches the Wyrm (KS99)</p>
+        </div>
+        <div>
+          <h2>Bearclaw Pinnacle</h2>
+            <p>Brothers (ENM75)</p>
+        </div>
+        <div>
+          <h2>Boneyard Gully</h2>
+            <p>Sheep in Antlion's Clothing (ENM75)</p>
+        </div>
+        <div>
+          <h2>Ghelsba Outpost</h2>
+          <p>Petrifying Pair (BS30)</p>
+        </div>
+        <div>
+          <h2>Horlais Peak</h2>
+            <p>Carapace Combatants (BS30)</p>
+            <p>Under Observation (BS40)</p>
+            <p>Double Dragonian (KS30)</p>
+        </div>
+        <div>
+          <h2>Qu'Bia Arena</h2>
+          <p>Undying Promise (BS40)</p>
+        </div>
+        <div>
+          <h2>Sacrifical Chamber</h2>
+          <p>Jungle Boogymen (BS60)</p>
+          <p>Amphibian Assault (BS60)</p>
+        </div>
+        <div>
+          <h2>Waughroon Shrine</h2>
+          <p>Royal Jelly (BS40)</p>
+          <p>Up in Arms (BS60)</p>
+          <p>Operation Desert Swarm (KS30)</p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BCNM-KSNM-ENM'
+}
+
+</script>
+
+<style scoped>
+
+  .bcnm-ksnm-enm {
+    grid-row: 2;
+    border: 1px solid white;
+    border-bottom: none;
+    border-right: none;
+    border-radius: .5rem;
+    padding: 3rem;
+    margin-left: 10rem;
+  }
+
+  .meat {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  p {
+    font-size: 0.9rem;
+    /* text-align: center; */
+  }
+
+</style>
--- src/views/FAQs.vue
+++ src/views/FAQs.vue
@@ -2,15 +2,26 @@
   <div class = "FAQs">
     <h2>FAQs</h2>
     <div class = "meat">
-      <p> Vanilla
-          FFXI provided the impossible task to beat the Shadow Lord at level 50 by forcing groups to combine melee and magic damage dealers.
-          Around the release of ROTZ the level cap was steadily raised to 75. ROTZ provided new missions and end game HNMs/Gods for the new 75
-          player base to come together. The new jobs added were mostly damage dealers. COP also brought different jobs together but this time
-          the challenge wasn't to be the best end game player you could be, but about how well you could master being a level 30, 40, 50, etc
-          player. After COP FFXI inroduced instanced zones (salvage), made melee burn, blm burn, smn burn, and mnk burn a more normal leveling
-          experience, allowed for players of all levels and all jobs to participate in events together(beseiged), and added jobs that filled
-          multiple roles (to varying degrees) in a party. In short, it made the game flatter in the tradition of WoW. I say all this to point
-          out why COP is different than the expansions that followed.</p>
+      <h2>Why a COP "era" server when there are four other "era" servers?</h2>
+        <p> Vanilla FFXI provided the impossible task to beat the Shadow Lord at level 50 by forcing groups to combine melee and magic damage dealers.
+            Around the release of ROTZ the level cap was steadily raised to 75.
+            ROTZ provided new missions and end game HNMs/Gods for the new 75
+            player base to come together. The new jobs added were mostly damage dealers.
+            COP also brought different jobs together but this time
+            the challenge wasn't to be the best end game player you could be, but about how well you could master being a level 30, 40, 50, etc
+            player. After COP FFXI inroduced instanced zones (salvage), made melee burn, blm burn, smn burn, and mnk burn a more normal leveling
+            experience, allowed for players of all levels and all jobs to participate in events together(beseiged), and added jobs that filled
+            multiple roles (to varying degrees) in a party. In short, it made the game flatter in the tradition of WoW. I say all this to point
+            out why COP is different than the expansions that followed.
+        </p>
+      <h2>Who Should Play On This Server?</h2>
+        <p>Anyone who thinks FFXI was at it's best during COP.</p>
+      <h2>What kind of server do you use?</h2>
+        <p>Operating System: Arch Linux.</p>
+        <p>Hardware: core i5 1140G7, 16GB LPDDR4X 4266MHz RAM, and a 512GB PCIe SSD.</p>
+        <p>Connection: 200mbps</p>
+      <h2>Could your toon data be lost?</h2>
+        <p>The database is backed up (at least) weekly to an online server and to an external hard drive</p>
     </div>
   </div>
 </template>
@@ -33,4 +44,5 @@ export default {
     padding: 3rem;
     margin-left: 10rem;
   }
+
 </style>
